e59f36698338216d2003058a4a25f7cc49f465e1
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-18 15:13:52 +0100

    Customer affordability tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -68,3 +68,14 @@ end
 def add_pet_to_customer(customer, new_pet)
   customer[:pets].push(new_pet)
 end
+
+# End of mandatory tests - tea-time!
+# 17 successful runs
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] >= new_pet[:price]
+    return true
+  else
+    return false
+  end
+end
