7ec0b4d19c1367807712d0a34ac0a8e0b7ddf68c
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-18 14:20:09 +0100

    Add pet to stock test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -45,6 +45,10 @@ end
 
 def remove_pet_by_name(pet_shop, name)
   pet_shop[:pets].delete_if { |pet|
-pet[:name] == name
+    pet[:name] == name
   }
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
 end
